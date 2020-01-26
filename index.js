9c9b060e421fb6a0ef5fd439887ce8ae4f2061fa
asawo <asawo@mercari.com>
2020-01-26 16:21:49 +0900

    added index.js to serve login.html

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("views/nodelogin"));
+
+app.get("/", (req, res) => {
+  res.end("Done");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at :${PORT}`);
+});
