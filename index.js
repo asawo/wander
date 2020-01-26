764adba0ffbdc7272be54bafcb152bf88f385d8f
asawo <asawo@mercari.com>
2020-01-26 16:39:54 +0900

    changed folder structure and changed login.html to index.html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("views/nodelogin/login.html"));
+app.use(express.static("views"));
 
 app.get("/", (req, res) => {
   res.end("Done");
