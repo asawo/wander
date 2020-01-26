5f4ba70316673fb5747289b5630b6cb2ddebbcb4
asawo <asawo@mercari.com>
2020-01-26 16:28:13 +0900

    selected login.html for express.static

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("views/nodelogin"));
+app.use(express.static("views/nodelogin/login.html"));
 
 app.get("/", (req, res) => {
   res.end("Done");
