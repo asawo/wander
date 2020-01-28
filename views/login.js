6594d9ffd335d390faad85d5799ef52accb8c578
asawo <asawo@mercari.com>
2020-01-28 21:52:49 +0900

    uninstalld mysql and display login.html file to client

---
+++ views/login.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "wander"
+});
+
+var app = express();
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
