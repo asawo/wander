aa7e789828392e3c7746dadf29c368b9b2ea9c74
asawo <asawo@mercari.com>
2020-01-28 22:38:51 +0900

    got rid of mysql require from login.js

--- views/login.js
+++ views/login.js
@@ -1,15 +1,7 @@
-var mysql = require("mysql");
 var express = require("express");
 var session = require("express-session");
 var bodyParser = require("body-parser");
 var path = require("path");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "wander"
-});
 
 var app = express();
 
@@ -20,5 +12,6 @@ app.use(
     saveUninitialized: true
   })
 );
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
