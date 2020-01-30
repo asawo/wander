c06d138273ceb9f3ac000ad90648f5d0c7aa8686
asawo <asawo@mercari.com>
2020-01-30 23:05:01 +0900

    serve views as static, added login route WIP

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.static("views"));
 
-app.get("/", (req, res) => {
-  res.end("Done");
+app.get("/login", (req, res) => {
+  let accounts = {
+    account: "arthur",
+    password: "secret hee hee"
+  };
+
+  res.json(accounts);
+});
+
+// {account: "arthur", password: "secret hee hee"}
+app.post("/login", (req, res) => {
+  let account = req.body.account;
+  let password = req.body.password;
+  console.log(account + " " + password);
+  res.send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server is listening at :${PORT}`);
 });
