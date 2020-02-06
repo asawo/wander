8beabfc26d5596eea6d0477e720a335bc4d90bf7
asawo <asawo@mercari.com>
2020-02-06 09:17:47 +0900

    swapped sign-in and sign-up forms, linked sign-up form to db

--- client/app.js
+++ client/app.js
@@ -2,11 +2,10 @@ const signInForm = document.querySelector('#signInForm')
 const signUpForm = document.querySelector('#signUpForm')
 
 // Sign in with form data
-function signIn(jsonData) {
-	fetch('../signin', {
+function signUp(jsonData) {
+	fetch('../signup', {
 		method: 'post',
 		headers: {
-			Accept: 'application/json',
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(jsonData)
@@ -19,6 +18,16 @@ function signIn(jsonData) {
 		})
 }
 
+signUpForm.addEventListener('submit', e => {
+	// event.preventDefault()
+	const signInFormData = {
+		username: e.target.elements[0].value,
+		password: e.target.elements[1].value
+	}
+
+	signUp(signInFormData)
+})
+
 signInForm.addEventListener('submit', e => {
 	event.preventDefault()
 	const signInFormData = {
@@ -26,9 +35,5 @@ signInForm.addEventListener('submit', e => {
 		password: e.target.elements[1].value
 	}
 
-	signIn(signInFormData)
-})
-
-signUpForm.addEventListener('submit', e => {
-	event.preventDefault()
+	// signIn(signInFormData)
 })
--- server/index.js
+++ server/index.js
@@ -4,13 +4,12 @@ const path = require('path')
 const userRoutes = require('./routes/users')
 const bodyParser = require('body-parser')
 const pgp = require('pg-promise')()
-const connectionString = 'postgres://localhost:5432/wander' // or process.env.blah
-const db = pgp(connectionString)
+const CONNECTION_STRING = 'postgres://localhost:5432/wander' // or process.env.blah
 
 // INSERT RECORDS
 // db.none(
-// 	'INSERT INTO accounts(username, password, email) VALUES($1, $2, $3) RETURNING id',
-// 	['asawo', 'fakepassword', 'fakeemail@gmail.com']
+// 	'INSERT INTO accounts(username, password) VALUES($1, $2) RETURNING id',
+// 	['asawo', 'fakepassword']
 // )
 // 	.then((data) => {
 // 		console.log(data)
@@ -24,20 +23,41 @@ const db = pgp(connectionString)
 // 	})
 // 	.catch(error => console.log(error))
 
+// Use user routes
 app.use('/server/routes/users', userRoutes)
 
 // Serve signin.html page
 app.use('/', express.static(path.join(__dirname, '../client')))
-
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/signin.html'))
+	res.sendFile(path.join(__dirname, '../client/signup.html'))
 })
 
 app.use(bodyParser.json())
 
-app.post('/signin', (req, res) => {
+const db = pgp(CONNECTION_STRING)
+
+// Register users
+app.post('/signup', (req, res) => {
 	let username = req.body.username
 	let password = req.body.password
+
+	db.oneOrNone('SELECT userid FROM users WHERE username = $1', [username]).then(
+		user => {
+			if (user) {
+				console.log('user already exists')
+				// Render the above message on page
+			} else {
+				db.none('INSERT INTO users(username,password) VALUES($1,$2)', [
+					username,
+					password
+				])
+					.then(() => {
+						console.log('SUCCESS')
+					})
+					.catch(error => console.log(error))
+			}
+		}
+	)
 
 	console.log(username)
 	console.log(password)
@@ -50,9 +70,3 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
 	console.log(`Server is listening at :${PORT}`)
 })
-
-// TODO:
-// - connect to db and run some queries
-// - maybe sequelize
-// - bcrypt - maybe later
-// -
