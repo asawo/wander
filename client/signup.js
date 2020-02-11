7731095a656b43afff77787fb3b14fff7df93a1b
asawo <asawo@mercari.com>
2020-02-11 11:20:44 +0900

    renamed app.js to signup, deleted old fetch function

--- client/signup.js
+++ client/signup.js
@@ -15,47 +15,12 @@ async function postData(url = '', data = {}) {
 	return await response.json()
 }
 
-function signUp(jsonData) {
-	fetch('../signup', {
-		method: 'post',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(jsonData)
-	})
-		.then(res => {
-			console.log(res)
-		})
-		.catch(e => {
-			console.log(e)
-		})
-}
-
-function signIn(jsonData) {
-	fetch('../signin', {
-		method: 'post',
-		redirect: 'follow',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(jsonData)
-	})
-		.then(res => {
-			if (res) {
-				console.log(res)
-			}
-		})
-		.catch(e => {
-			console.log(e)
-		})
-}
-
 signUpForm.addEventListener('submit', e => {
 	const signUpFormData = {
 		username: e.target.elements[0].value,
 		password: e.target.elements[1].value
 	}
-	// signUp(signUpFormData)
+
 	postData('../signup', signUpFormData).then(data => {
 		console.log(data)
 	})
@@ -70,5 +35,4 @@ signInForm.addEventListener('submit', e => {
 	postData('../signin', signInFormData).then(data => {
 		console.log(data)
 	})
-	// signIn(signInFormData)
 })
