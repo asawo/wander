060c515fc5ea569183620b32cb53ccf89336b399
asawo <asawo@mercari.com>
2020-02-20 21:30:34 +0900

    added test to connect to db

--- test/test-server.js
+++ test/test-server.js
@@ -1,12 +1,17 @@
-var chai = require('chai')
-var chaiHttp = require('chai-http')
-var server = require('../server/index.js')
-var should = chai.should()
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../server/index.js')
+const pgp = require('pg-promise')()
+const CONNECTION_STRING =
+	process.env.DATABASE_URL || 'postgres://localhost:5432/wander'
+const db = pgp(CONNECTION_STRING)
+
+const should = chai.should()
 
 chai.use(chaiHttp)
 
 describe('page load', function() {
-	it('should return 200 for / GET', function(done) {
+	it('Should return 200 for / GET', function(done) {
 		chai
 			.request(server)
 			.get('/')
@@ -14,6 +19,17 @@ describe('page load', function() {
 				res.should.have.status(200)
 				done()
 			})
+	})
+})
+
+describe('Check DB', function() {
+	it('Connecting to psql db', function(done) {
+		db.any('SELECT username FROM users')
+			.then(res => {
+				res[0].username.should.equal('polly')
+				done()
+			})
+			.catch(e => console.log(e))
 	})
 })
 
