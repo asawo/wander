634bd225a98faf322a433c5664606b8185571a12
asawo <asawo@mercari.com>
2020-02-24 10:07:35 +0900

    added post and checkImage function

--- client/add-doggos.js
+++ client/add-doggos.js
@@ -1,5 +1,21 @@
 const newDog = document.querySelector('#newDog')
 
+async function postData(url = '', data = {}) {
+	const response = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		redirect: 'follow',
+		body: JSON.stringify(data)
+	})
+
+	let status = response.status
+	let res = await response.json()
+
+	return { status: status, response: res }
+}
+
 newDog.addEventListener('submit', e => {
 	e.preventDefault()
 
@@ -7,19 +23,29 @@ newDog.addEventListener('submit', e => {
 
 	const doggoProfile = {
 		doggoName: e.target.elements[0].value,
-		doggoImage: e.target.elements[1].value,
+		doggoImage: e.target.elements[1].files[0],
 		description: e.target.elements[2].value
 	}
 
-	if (signUpFormData.password === secondPassword) {
-		postData('../users/add-doggo', doggoProfile).then(data => {
-			if (data.status === 200) {
-				$('.doggo-created').show() // success label for creating doggo
-			} else {
-				$('.doggo-exists').show() // doggo exists!
-			}
-		})
-	} else {
-		$('.something-went-wrong').show() // something went wrong!
+	const checkImage = file => {
+		let imageType = /image.*/
+		if (!file.type.match(imageType)) {
+			throw 'Choose an image file!'
+		} else if (!file) {
+			throw 'Make sure to upload a pic!'
+		} else {
+			return true
+		}
 	}
+
+	console.log(checkImage(doggoProfile.doggoImage))
+	console.log(doggoProfile)
+
+	// postData('../users/add-doggo', doggoProfile).then(data => {
+	// 	if (data.status === 200) {
+	// 		$('.doggo-created').show() // success label for creating doggo
+	// 	} else {
+	// 		$('.doggo-exists').show() // doggo exists!
+	// 	}
+	// })
 })
