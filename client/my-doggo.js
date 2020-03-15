1b5cf5d9fce7cfdb04fb2a5b5e766054c41e5871
asawo <asawo@mercari.com>
2020-03-15 19:35:28 +0900

    set max-width of doggo card to 660px

--- client/my-doggo.js
+++ client/my-doggo.js
@@ -16,12 +16,12 @@ const myDoggos = document.querySelector('#myDoggos')
 
 const addDoggos = (doggoName, description, imageurl) => {
 	myDoggos.innerHTML += `
-	<div class="doggos text-left container" style="background-color: #ffffff;">
+	<div class="doggos text-left container" style="background-color: #ffffff; max-width: 660px;">
 		<div class="mt-5 mx-auto">
-			<img src=${imageurl} class="pt-3" alt="Doggo image goes here" width="100%">
+			<img src=${imageurl} class="pt-3" alt="Doggo image" width="100%">
 			<div class="card-body">
-				<h5 class="card-title">${doggoName}</h5>
-				<p class="card-text">${description}</p>
+				<h1 class="dogName">${doggoName}</h1>
+				<p class="dogDescription container">${description}</p>
 				<span>
 					<a href="#" class="btn btn-primary">Pat</a>
 					<a href="#" class="btn btn-secondary">Like</a>
