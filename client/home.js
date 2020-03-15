aafc5589a7c1bf136edc0ead7442c332546d2e31
asawo <asawo@mercari.com>
2020-03-15 21:26:13 +0900

    adjusted doggo cards to break and wrap overflow text

--- client/home.js
+++ client/home.js
@@ -2,13 +2,13 @@ const timeline = document.querySelector('#timeline')
 
 const addDoggos = (doggoName, description, imageurl) => {
 	timeline.innerHTML += `
-	<div class="doggos text-left container" style="background-color: #ffffff; max-width: 660px;">
+	<div class="doggos text-left text-wrap container" style="overflow-wrap: break-word; background-color: #ffffff; max-width: 660px;">
 		<div class="mt-5 mx-auto">
 			<img src=${imageurl} class="pt-3" alt="Doggo image" width="100%">
 			<div class="card-body">
 				<h1 class="dogName">${doggoName}</h1>
-				<p class="dogDescription container">${description}</p>
-				<span>
+				<p class="dogDescription">${description}</p>
+				<span class="mt-3">
 					<a href="#" class="btn btn-primary">Pat</a>
 					<a href="#" class="btn btn-secondary">Like</a>
 				</span>
