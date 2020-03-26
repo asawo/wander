b9c420e784b98f707f23e40d154c271a7925b409
asawo <asawo@mercari.com>
2020-03-26 10:44:02 +0900

    added edit and delete buttons in my doggos

--- client/my-doggo.js
+++ client/my-doggo.js
@@ -17,8 +17,14 @@ const myDoggos = document.querySelector('#myDoggos')
 const addDoggos = (doggoName, description, imageurl) => {
 	myDoggos.innerHTML += `
 	<div class="doggos text-left text-wrap container" style="overflow-wrap: break-word; background-color: #ffffff; max-width: 660px;">
-		<div class="mt-5 mx-auto">
-			<img src=${imageurl} class="pt-3" alt="Doggo image" width="100%">
+		<div class="mt-5 pt-3 mx-auto">
+		<div class="text-right">
+			<span class="mt-3 text-center">
+				<a href="#" class="btn btn-info">Edit <i class="fa fa-edit"></i></a>
+				<a href="#" class="btn btn-danger">Delete <i class="fa fa-trash"></i></a>
+			</span>
+		</div>
+			<img src=${imageurl} class="doggoImage mt-3" alt="Doggo image" width="100%">
 			<div class="card-body">
 				<h1 class="dogName">${doggoName}</h1>
 				<p class="dogDescription">${description}</p>
