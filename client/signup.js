1cc8418b519f93da4b12a2dd37e59259b81f2536
asawo <asawo@mercari.com>
2020-04-07 21:51:53 +0900

    added parenthesis to a single param being passed into a function

--- client/signup.js
+++ client/signup.js
@@ -5,10 +5,10 @@ async function postData(url = '', data = {}) {
 	const response = await fetch(url, {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json',
 		},
 		redirect: 'follow',
-		body: JSON.stringify(data)
+		body: JSON.stringify(data),
 	})
 
 	const status = response.status
@@ -18,13 +18,13 @@ async function postData(url = '', data = {}) {
 }
 
 // Log in
-signUpForm.addEventListener('submit', e => {
+signUpForm.addEventListener('submit', (e) => {
 	e.preventDefault()
 	document.querySelector('.alert').style.display = 'none'
 
 	const signUpFormData = {
 		username: e.target.elements[0].value,
-		password: e.target.elements[1].value
+		password: e.target.elements[1].value,
 	}
 
 	const secondPassword = e.target.elements[2].value
@@ -33,7 +33,7 @@ signUpForm.addEventListener('submit', e => {
 		document.querySelector('.wrong-password').style.display = 'block'
 	} else {
 		postData('../signup', signUpFormData)
-			.then(data => {
+			.then((data) => {
 				if (data.status === 200) {
 					document.querySelector('.sign-up-success').style.display = 'block'
 				} else {
@@ -41,34 +41,34 @@ signUpForm.addEventListener('submit', e => {
 				}
 				return postData('../signin', signUpFormData)
 			})
-			.then(data => {
+			.then((data) => {
 				if (data.status === 301) {
 					setTimeout(() => {
 						window.location.replace('/users/home')
 					}, 1000)
 				}
 			})
-			.catch(error => console.log({ error }))
+			.catch((error) => console.log({ error }))
 	}
 })
 
 // Hides all alerts on keyup
-signUpForm.addEventListener('keyup', e => {
-	document.querySelectorAll('.alert').forEach(alert => {
+signUpForm.addEventListener('keyup', (e) => {
+	document.querySelectorAll('.alert').forEach((alert) => {
 		alert.style.display = 'none'
 	})
 })
 
 // Registration
-signInForm.addEventListener('submit', e => {
+signInForm.addEventListener('submit', (e) => {
 	e.preventDefault()
 
 	const signInFormData = {
 		username: e.target.elements[0].value,
-		password: e.target.elements[1].value
+		password: e.target.elements[1].value,
 	}
 
-	postData('../signin', signInFormData).then(data => {
+	postData('../signin', signInFormData).then((data) => {
 		if (data.status === 301) {
 			window.location.replace('/users/home')
 		} else {
@@ -77,6 +77,6 @@ signInForm.addEventListener('submit', e => {
 	})
 })
 
-signInForm.addEventListener('keyup', e => {
+signInForm.addEventListener('keyup', (e) => {
 	document.querySelector('.sign-in-alert').style.display = 'none'
 })
